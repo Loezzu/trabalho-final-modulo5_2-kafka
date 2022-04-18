7816e09b70e80bcccd942c104cb354028012798f
Luiz Souza <luixsouza12@outlook.com>
2022-04-18 11:20:28 -0300

    nome da controller para log alterado

--- src/main/java/com/tindev/tindevapi/controller/contact/ContactController.java
+++ src/main/java/com/tindev/tindevapi/controller/contact/ContactController.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.tindev.tindevapi.enums.Tipo;
 import com.tindev.tindevapi.service.ContactService;
 import com.tindev.tindevapi.service.ProdutorService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contato")
 @RequiredArgsConstructor
+@Api(value = "7 - Contact API", produces = MediaType.APPLICATION_JSON_VALUE, tags = {"7 - Contact API"})
 public class ContactController {
 
     private final ContactService contactService;
---
+++ src/main/java/com/tindev/tindevapi/controller/log/LogController.java
@@ -0,0 +1,26 @@
+package com.tindev.tindevapi.controller.log;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tindev.tindevapi.dto.log.LogDTO;
+import com.tindev.tindevapi.service.ProdutorService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+@RequiredArgsConstructor
+@Api(value = "6 - Log API", produces = MediaType.APPLICATION_JSON_VALUE, tags = {"6 - Log API"})
+public class LogController {
+
+    private final ProdutorService produtorService;
+
+    @PostMapping("/registro")
+    private void enviar(LogDTO logDTO) throws JsonProcessingException {
+        produtorService.enviarLog(logDTO);
+    }
+
+}
